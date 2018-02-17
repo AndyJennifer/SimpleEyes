83bca66c484085d3b6d0cd12fa49c4cdf7c219eb
AndyJennifer <1225868370@qq.com>
2018-02-17 23:08:13 +0800

    修改了mediaController控制界面 修改了6.0上phoneWindow 无法通过反射获取的问题

--- app/src/main/java/com/jennifer/andy/simpleeyes/player/PolicyCompat.java
+++ app/src/main/java/com/jennifer/andy/simpleeyes/player/PolicyCompat.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.Window;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 /**
  * Author:  andy.xwt
@@ -15,12 +16,27 @@ import java.lang.reflect.Constructor;
 public class PolicyCompat {
 
     private static final String PHONE_WINDOW_CLASS_NAME = "com.android.internal.policy.PhoneWindow";
+    private static final String POLICY_MANAGER_CLASS_NAME = "com.android.internal.policy.PolicyManager";
 
+    /**
+     * 6.0通过反射拿不到phoneWindow
+     */
     public static Window createPhoneWindow(Context context) {
         try {
             Class<?> clazz = Class.forName(PHONE_WINDOW_CLASS_NAME);
             Constructor c = clazz.getConstructor(Context.class);
             return (Window) c.newInstance(context);
+        } catch (Exception e) {
+           return  makeNewWindow(context);
+        }
+    }
+
+    private static Window makeNewWindow(Context context) {
+        Class<?> clazz = null;
+        try {
+            clazz = Class.forName(POLICY_MANAGER_CLASS_NAME);
+            Method m = clazz.getMethod("makeNewWindow", Context.class);
+            return (Window) m.invoke(null, context);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
