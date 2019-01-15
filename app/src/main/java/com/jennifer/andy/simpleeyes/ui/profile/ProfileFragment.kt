1dfa46808c5b99f239ba9849bf040d83a33f5353
AndyJennifer <1225868370@qq.com>
2019-01-15 18:58:32 +0800

    添加了我的界面

--- app/src/main/java/com/jennifer/andy/simpleeyes/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/jennifer/andy/simpleeyes/ui/profile/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.jennifer.andy.simpleeyes.ui.profile
 
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import com.jennifer.andy.simpleeyes.R
 import com.jennifer.andy.simpleeyes.ui.base.BaseFragment
 import com.jennifer.andy.simpleeyes.ui.profile.presenter.ProfilePresenter
 import com.jennifer.andy.simpleeyes.ui.profile.view.ProfileView
+import com.jennifer.andy.simpleeyes.utils.kotlin.bindView
 
 /**
  * Author:  andy.xwt
@@ -14,6 +16,7 @@ import com.jennifer.andy.simpleeyes.ui.profile.view.ProfileView
 
 class ProfileFragment : BaseFragment<ProfileView, ProfilePresenter>() {
 
+    private val mRecycler: RecyclerView by bindView(R.id.rv_profile_recycler)
 
     companion object {
         @JvmStatic
